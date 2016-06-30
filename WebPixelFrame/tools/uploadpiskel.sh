1dc195ca896a4205f9da5155e3574d4c01d94845
Alan Steremberg <alan.steremberg@gmail.com>
2016-06-29 21:07:57 -0700

    fixes to the gallery

--- WebPixelFrame/tools/uploadpiskel.sh
+++ WebPixelFrame/tools/uploadpiskel.sh
@@ -6,12 +6,18 @@ rm -f p/js/piskel-packageda.js
 php tools/convertdir.php
 cd data
 curl -F "file=@$PWD/gallery.html;filename=/gallery.html" http://admin:admin@10.0.2.29/edit
+curl -F "file=@$PWD/gallery.js.gz;filename=/gallery.js.gz" http://admin:admin@10.0.2.29/edit
+curl -F "file=@$PWD/gallery.css.gz;filename=/gallery.css.gz" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/piskel-app-previewcard.css;filename=/piskel-app-previewcard.css" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/piskel-app.css;filename=/piskel-app.css" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/piskel-utils.js;filename=/piskel-utils.js" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/piskel-image-scale.js;filename=/piskel-image-scale.js" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/piskel-animated-preview.js;filename=/piskel-animated-preview.js" http://admin:admin@10.0.2.29/edit
 curl -F "file=@$PWD/checkback.png;filename=/checkback.png" http://admin:admin@10.0.2.29/edit
+curl -F "file=@$PWD/iconmonstr-lock-3-icon.svg;filename=/iconmonstr-lock-3-icon.svg" http://admin:admin@10.0.2.29/edit
+#
+# to delete a file:
+# curl -X  "DELETE" -F "path=/gallery.js" http://admin:admin@10.0.2.29/edit
 gzip po/71	
 gzip po/73
 gzip po/75
